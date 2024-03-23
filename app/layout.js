b6864b3db93ed874c34bac9122bda72c1796009e
unknown <you@example.com>
2024-03-23 15:13:22 +0600

    Fix font issues

--- app/layout.js
+++ app/layout.js
@@ -20,27 +20,31 @@ const inter = Inter({
 
 const poppins = Poppins({
   subsets: ["latin"],
+  variable: "--font-poppins",
+  display: "swap",
   weight: ["400", "500", "600", "700", "800"],
 });
 
 const noto = Noto_Serif_Bengali({
   subsets: ["latin", "bengali"],
+  variable: "--font-noto",
+  display: "swap",
   weight: ["400", "500", "600", "700", "800"],
 });
 
 const montserrat = Montserrat({
   subsets: ["latin"],
+  variable: "--font-montserrat",
+  display: "swap",
   weight: ["400", "500", "600", "700", "800"],
 });
 
 const figtree = Figtree({
   subsets: ["latin"],
+  variable: "--font-figtree",
+  display: "swap",
   weight: ["400", "500", "600", "700", "800"],
 });
-// const blog = Website_Blog_Subheading({
-//   subsets: ["latin"],
-//   weight: ["400", "500", "600", "700", "800"],
-// });
 
 export const metadata = {
   title: "MCQ Genius",
@@ -51,7 +55,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${inter.variable} ${poppins.className} ${noto.className} ${montserrat.className}`}
+        className={`${inter.variable} ${poppins.variable} ${noto.variable} ${montserrat.variable}`}
       >
         <Navbar />
         <Header />
